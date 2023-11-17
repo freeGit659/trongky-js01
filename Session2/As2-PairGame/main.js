4c8e2dbac7025867bcd72e3653636c90ade46b95
Lionel Cao <128696060+lionelcao659@users.noreply.github.com>
2023-11-17 22:38:28 +0700

    Session2

--- Session2/As2-PairGame/main.js
+++ Session2/As2-PairGame/main.js
@@ -10,10 +10,17 @@ var cardColor = {
     brown: "#8B4513",
     blueBlack: "#191970"
 }
+
 var cardRandom = { black: 0, red: 0, green: 0, blue: 0, yellow: 0, pink: 0, sliver: 0, redBlack: 0, brown: 0, blueBlack: 0 }
-createBlock(20);
-createCoverBlock(20);
+
+var scoreCur = 10000;
 var colorTemp = null;
+var elementTemp;
+var waitingFlag = false;
+
+createBlock(20);
+score(null);
+
 function block(){
     var p = document.getElementsByClassName("block");
     for(let element = 0; element < p.length; element++) {
@@ -24,6 +31,7 @@ function block(){
         p[element].style.border = "2px solid #dadada"
     }
 }block();
+
 function blockCover(){
     var p = document.getElementsByClassName("blockCover");
     for(let element = 0; element < p.length; element++) {
@@ -33,18 +41,29 @@ function blockCover(){
         p[element].style.borderRadius = "5px";
         p[element].style.border = "2px solid #dadada"
         p[element].addEventListener("click", event => {
-            if (logic(p[element].value)) p[element].style.opacity = "0";
+            if (!waitingFlag){
+            let choice = logic(p[element].value);
+            if (choice) {
+                p[element].style.opacity = "0";
+                elementTemp = element;
+            }
             else {
-                    p[element].style.opacity = "0";
-                    setTimeout(function (){
-                p[element].style.opacity = "1";
-            },2000);
+                p[element].style.opacity = "0";
+                waitingFlag = true;
+                setTimeout(function (){
+                    waitingFlag = false;
+                    p[element].style.opacity = "1";
+                    p[elementTemp].style.opacity = "1";
+                    elementTemp = null;
+                },1000);
             }
             console.log("choice: ", p[element].value);
-            console.log(logic(p[element].value));
+            console.log(elementTemp);
+        }
             });
     }
 }blockCover();
+
 function createBlock(num){
     for(let i = 1; i <= num; i++){
         var div = document.createElement("div");
@@ -67,11 +86,6 @@ function createBlock(num){
     }
 }
 
-function createCoverBlock(num){
-    for(let i = 1; i <= num; i++){
-
-    }
-}
 function createColorCard(){
     while(Object.keys(cardColor).length > 0){
     var keys = Object.keys(cardColor);
@@ -85,21 +99,6 @@ function createColorCard(){
     }
 }
 
-function logic(color){
-    console.log("temp: ",colorTemp);
-    if (colorTemp === null) {
-        colorTemp = color;
-        return true;
-    }
-    if (colorTemp === color) {
-        colorTemp = null;
-        return true;
-    }
-    else {
-        colorTemp = null;
-        return false;
-    }
-}
 function RGBToHex(rgb) {
     let sep = rgb.indexOf(",") > -1 ? "," : " ";
     rgb = rgb.substr(4).split(")")[0].split(sep);
@@ -116,4 +115,29 @@ function RGBToHex(rgb) {
       b = "0" + b;
   
     return "#" + r + g + b;
-  }
+}
+
+function logic(color){
+    console.log("temp: ",colorTemp);
+    if (colorTemp === null) {
+        colorTemp = color;
+        return true;
+    }
+    if (colorTemp === color) {
+        score(true);
+        colorTemp = null;
+        return true;
+    }
+    else {
+        colorTemp = null;
+        score(false);
+        return false;
+    }
+}
+
+function score(match){
+    var p = document.getElementById('score');
+    if(match == true) scoreCur += 1000;
+    else if (match ==false)  scoreCur -= 500;
+    p.innerText = scoreCur.toString();
+}
